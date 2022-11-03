81bab55859bf44d5c320b90463037f7da30c567a
Ihs4nn <dfihsan@gmail.com>
2022-11-03 14:24:49 +0000

    Adding code to Board Model

---
+++ models/board.model.js
@@ -0,0 +1,21 @@
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const db = require('../db/db')
+
+class Board extends Model { }
+
+Board.init({
+    type:{
+        type: DataTypes.STRING
+    },
+    description:{
+        type: DataTypes.STRING
+    },
+    rating:{
+        type: DataTypes.INTGER
+    }
+},{
+    sequelize: db,
+    modelName: 'Board'
+})
+
+module.exports = Board
